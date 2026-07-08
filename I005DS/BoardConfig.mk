5b2ede00bb1d04c6bef591a002f5ed5c8bd12b98
Samuel Kendall <kinguser981@gmail.com>
2026-07-08 12:49:12 +0000

    I005DS: add TW_MAX_BRIGHTNESS and other TWRP flags to fix orangefox build error

--- I005DS/BoardConfig.mk
+++ I005DS/BoardConfig.mk
@@ -27,6 +27,9 @@
 -include $(DEVICE_PATH)/BoardConfigCommon.mk
 
 # TWRP device-specific build flags
+TW_HAPTICS_TSPDRV := true
+TW_MAX_BRIGHTNESS := 1023
+TW_NO_SCREEN_BLANK := true
 TW_LOAD_VENDOR_MODULES := "adsp_loader_dlkm.ko aw8697.ko focaltech_fts_rog.ko focaltech_fts_rog2.ko q6_dlkm.ko texfat.ko tntfs.ko"
 
 #
